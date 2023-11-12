43fb7ee4776b36e36cf82d7d3ea9dadebe4b395c
hanseulhee <3021062@gmail.com>
2023-11-13 02:55:43 +0900

    apply book card components

---
+++ src/components/Card/BookCard.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+import theme from 'styles/theme';
+
+interface Props {
+  img: string;
+  title: string;
+  author: string;
+}
+
+function BookCard({ img, title, author }: Props) {
+  return (
+    <StyledWrapper>
+      <StyledImgWrapper>
+        <StyledImg src={img} alt="책 표지" />
+      </StyledImgWrapper>
+
+      <StyledContentWrapper>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledAuthor>{author}</StyledAuthor>
+      </StyledContentWrapper>
+    </StyledWrapper>
+  );
+}
+
+export default BookCard;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledImgWrapper = styled.div`
+  width: 200px;
+`;
+
+const StyledImg = styled.img`
+  width: 100%;
+`;
+
+const StyledContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 0.6rem;
+  gap: 2px;
+`;
+
+const StyledTitle = styled.span`
+  font-size: 1.2rem;
+  font-weight: ${theme.fontWeight.bold};
+`;
+
+const StyledAuthor = styled.p`
+  font-weight: ${theme.fontWeight.light};
+`;
