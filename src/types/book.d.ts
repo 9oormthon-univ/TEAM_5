bfcd81d9f5e6811202e14da58aca596fd2780817
hanseulhee <3021062@gmail.com>
2023-11-18 18:53:08 +0900

    modify types

--- src/types/book.d.ts
+++ src/types/book.d.ts
@@ -1,6 +1,16 @@
+interface Book {
+  books: BookList[];
+  clouds: CloudsList[];
+}
+
 interface BookList {
   id: number;
   title: string;
   authors: string;
   imgUrl: string;
+}
+
+interface CloudsList {
+  id: number;
+  style: number;
 }
